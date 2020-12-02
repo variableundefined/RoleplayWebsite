966f97860e277c8fea398a622a970cfcf6d7d9b4
variableundefined <40092670+variableundefined@users.noreply.github.com>
2020-12-02 16:58:13 -0500

    Update settings for more secure practice

--- spellslist/settings.py
+++ spellslist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DRASEWEB_SECRET_KEY']
+SECRET_KEY = os.getenv("DRASEWEB_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DRASEWEB_DEBUG']
+DEBUG = os.getenv('DRASEWEB_DEBUG', "False") == "True"
 
-ALLOWED_HOSTS = os.environ['DRASEWEB_ALLOWED_HOSTS']
+ALLOWED_HOSTS = os.getenv('DRASEWEB_ALLOWED_HOSTS', "127.0.0.1, localhost").split(",")
 
 
 # Application definition
