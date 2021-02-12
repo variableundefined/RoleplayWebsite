344899755ae0361d4392b3daeba20326dadfb7f5
variableundefined <40092670+variableundefined@users.noreply.github.com>
2021-02-11 23:20:46 -0500

    Update settings.py

--- spellslist/settings.py
+++ spellslist/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DRASEWEB_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DRASEWEB_DEBUG', "False") == "True"
 
-ALLOWED_HOSTS = os.getenv('DRASEWEB_ALLOWED_HOSTS', "127.0.0.1, localhost").split(",")
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "127.0.0.1, localhost").split(",")
 
 
 # Application definition
